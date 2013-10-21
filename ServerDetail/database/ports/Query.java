27e367161ec8d586ed82e1cb55c15afec6f33385
Florian Fagniez <f.fagniez@dental-moving.com>
2013-10-21 17:30:49 +0200

    This works ?

--- ServerDetail/database/ports/Query.java
+++ ServerDetail/database/ports/Query.java
@@ -1,9 +1,18 @@
 package database.ports;
 
 import elements.SynchronPort;
+import enumerations.InterfaceType;
+import enumerations.VisibilityType;
+
 
 // Change port type ! 
 
 public class Query extends SynchronPort {
+
+	public Query(String name, InterfaceType interfaceT,
+			VisibilityType visibilityT) {
+		super(name, interfaceT, visibilityT);
+		// TODO Auto-generated constructor stub
+	}
 
 }
